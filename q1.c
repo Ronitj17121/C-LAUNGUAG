a6d26d454711424ec2f4285fbe97134e60f6a968
RONIT JARIWALA <ronitjariwala2006@gmail.com>
2024-07-01 11:18:54 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+int x,y;
+
+		printf("Enter first number");
+		scanf("%d",&x);
+		printf("Enter secound number");
+		scanf("%d",&y);
+		
+		(x,y)?printf("x is minimum"):printf("y is minimum");
+}
